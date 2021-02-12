56782e18161a68701776cfbd7efb538fa7e67bf1
andresav567 <andres_a09@hotmail.com>
2021-02-12 16:15:16 -0300

    toma gil

---
+++ src/app/city/city.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CityComponent } from './city/city.component';
+
+
+
+@NgModule({
+  declarations: [CityComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CityModule { }
